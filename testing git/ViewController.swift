3774247c1375d717295bd4d41212136a65cdcf2d
mark <mw.waspo@gmail.com>
2020-01-10 08:37:17 -0700

    Initial Commit

---
+++ testing git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testing git
+//
+//  Created by Mark Wilson on 1/10/20.
+//  Copyright © 2020 Mark Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
